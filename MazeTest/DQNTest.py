abb98beb5fd23466023827797cf05d9cbe4268c2
imnath09 <bjsdtc09@126.com>
2022-03-03 22:59:42 +0800

    separate train and test

--- MazeTest/DQNTest.py
+++ MazeTest/DQNTest.py
@@ -1,5 +1,8 @@
 
 import sys
+
+from cv2 import batchDistance
+from grpc import ssl_server_certificate_configuration
 sys.path.append('..')
 
 from Maze.unrenderedmaze import *
@@ -13,89 +16,60 @@ from Common.dmdp_actions import *
 import time
 
 STAT_GAP = 1000
-TOTAL_EPISODE = 50 * STAT_GAP
+#TOTAL_EPISODE = 50 * STAT_GAP
+TOTAL_ITER = 100
 
 
-def DQNTest():
-    suc_matrix = np.array([[0, 0],[0, 0]]) # 幕抵达矩阵
-    suc = [[], []] # 成功率
-
-    t_len = np.array([0, 0]) # 平均长度
-    avr_len = [[], []] # 
-
-    x = []
-    endpoints = np.zeros((MAZE_HEIGHT + 2, MAZE_WIDTH + 2), dtype = int)
-
-    suc_len = [[[], []], [[], []]] # 路径长度
-
+def Batch(isTrain : bool):
+    path_len = [] # 记录成功时的路径长度
     DQN_step = 0
-
-    print(time.ctime())
-    for episode in range(1, TOTAL_EPISODE + 1):
+    for episode in range(1, STAT_GAP + 1):
         observation = env.reset()
-        explore = False # 探索
-
         # one trial
         while True:
-            action = RL.choose_action(observation)
+            action = RL.choose_action(observation) if isTrain else RL.action(observation)
             observation_, reward, done, info = env.step(action)
-            RL.store_transition(observation, action, reward, observation_) # , done)
-            if True: # (DQN_step > 500) and (DQN_step % 5 == 0):
-                RL.learn()
+            if isTrain:
+                RL.store_transition(observation, action, reward, observation_) # , done)
+                if True: # (DQN_step > 500) and (DQN_step % 5 == 0):
+                    RL.learn()
             observation = observation_
             env.render()
 
-            explore |= RL.random_action
             if done:
-                endpoints[tuple(observation + [1, 1])] += 1
-                i = 0 if explore else 1 # 0是探索，1是利用
-                suc_matrix[i][0] += 1 # 总次数 探索/利用
-                suc_len[i][0].append(episode)
-
-                if reward == ARRIVE_REWARD: # 抵达目的地
-                    suc_matrix[i][1] += 1 # 成功次数 探索/利用
-                    suc_len[i][1].append(env.cur_path.__len__()) # 路径长度 探索/利用
-                    t_len[i] += env.cur_path.__len__()
-
+                if reward == ARRIVE_REWARD:
+                    len = len(env.cur_path)
+                    path_len.append(len)
                     if env.new_sln:
-                        info = "path length:{}".format(env.cur_path.__len__())
+                        info = "path length:{}".format(len)
                         for n in env.cur_path:
                            info = ("{}->{}".format(info, str(n)))
                         print("episode{} {}".format(episode, info))
-                else: # 没有抵达目的地
-                    suc_len[i][1].append(0)
                 break
             DQN_step += 1
         # enf of while(one trial)
+    # enf of for(trial process)
+    return path_len
 
-        if episode % STAT_GAP == 0:
-            # 探索成功率
-            c0 = 0 if suc_matrix[0][1] == 0 else round(suc_matrix[0][1] / suc_matrix[0][0], 4)
-            # 利用成功率
-            c1 = 0 if suc_matrix[1][1] == 0 else round(suc_matrix[1][1] / suc_matrix[1][0], 4)
-            suc[0].append(c0)
-            suc[1].append(c1)
+def core():
+    suc_rate = [] # 成功率
+    avr_len = [] # 平均长度
 
-            al0 = 0 if t_len[0] == 0 else round(t_len[0] / suc_matrix[0][1], 4) # 探索平均长度
-            al1 = 0 if t_len[1] == 0 else round(t_len[1] / suc_matrix[1][1], 4) # 利用平均长度
-            avr_len[0].append(al0)
-            avr_len[1].append(al1)
+    for _ in range(TOTAL_ITER):
+        train = Batch(isTrain = True)
 
-            x.append(episode)
-
-            if True:
-                print("eps:{}; explore:{}/{}={}, len:{}; exploit:{}/{}={}, len:{};".format(
-                episode,
-                suc_matrix[0][1], suc_matrix[0][0], c0, al0,
-                suc_matrix[1][1], suc_matrix[1][0], c1, al1))
-
-            suc_matrix *= 0
-            t_len *= 0
-    # enf of for(train process)
-    display(endpoints, x, suc, avr_len, suc_len)
+        test = Batch(isTrain = False)
+        suc_rate.appen(len(suc_rate) / STAT_GAP)
+        avr_len.append(sum(test) / STAT_GAP)
 
 
-def display(endpoints, x, suc, avr_len, suc_len):
+
+def display(path_len):
+
+    pass
+
+
+def display_old(endpoints, x, suc, avr_len, suc_len):
     print('game over', time.ctime())
     print(endpoints)
     RL.plot_cost()
@@ -128,7 +102,11 @@ if __name__ == '__main__':
     rendered = args.render
 
     env = GymMaze() if rendered else UnrenderedMaze()
-    RL = DeepQNetwork(env.action_space.__len__(), n_features = 2)#, memory_size = 2000)
-    DQNTest()
+    RL = DeepQNetwork(len(env.action_space), n_features = 2, memory_size = 2000, e_greedy = 0.5)
+    print("begin at ", time.ctime())
+
+    print("finish at ", time.ctime())
+
+
 
 
