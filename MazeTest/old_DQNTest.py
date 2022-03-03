f609e64379d12cdfd40b2ce7a87bc063e52479ae
imnath09 <bjsdtc09@126.com>
2022-03-03 17:03:58 +0800

    主要修改DQN训练/测试代码

--- MazeTest/old_DQNTest.py
+++ MazeTest/old_DQNTest.py
@@ -4,8 +4,7 @@ sys.path.append('..')
 
 from Maze.unrenderedmaze import *
 from Maze.gymmaze import GymMaze
-from Algorithm.TFDQN import DeepQNetwork
-#from Algorithm.DoubleDQN import DeepQNetwork
+from Algorithm.TorchDQN import DeepQNetwork
 
 import matplotlib.pyplot as plt
 import argparse
@@ -27,36 +26,35 @@ def DQNTest():
     x = []
     endpoints = np.zeros((MAZE_HEIGHT + 2, MAZE_WIDTH + 2), dtype = int)
 
-    suc_length = [[[], []], [[], []]] # 路径长度
+    suc_len = [[[], []], [[], []]] # 路径长度
 
     DQN_step = 0
 
     print(time.ctime())
     for episode in range(1, TOTAL_EPISODE + 1):
-        #print("episode: {}".format(episode))
         observation = env.reset()
         explore = False # 探索
 
+        # one trial
         while True:
-            #print("step: {}".format(step))
-            env.render()
             action = RL.choose_action(observation)
-            observation_, reward, done, info = env.step(actions(action))
-            RL.store_transition(observation, action, reward, observation_, done)
-            if (DQN_step > 500) and (DQN_step % 5 == 0):
+            observation_, reward, done, info = env.step(action)
+            RL.store_transition(observation, action, reward, observation_) # , done)
+            if True: # (DQN_step > 500) and (DQN_step % 5 == 0):
                 RL.learn()
             observation = observation_
+            env.render()
 
             explore |= RL.random_action
             if done:
                 endpoints[tuple(observation + [1, 1])] += 1
                 i = 0 if explore else 1 # 0是探索，1是利用
                 suc_matrix[i][0] += 1 # 总次数 探索/利用
-                suc_length[i][0].append(episode)
+                suc_len[i][0].append(episode)
 
                 if reward == ARRIVE_REWARD: # 抵达目的地
                     suc_matrix[i][1] += 1 # 成功次数 探索/利用
-                    suc_length[i][1].append(env.cur_path.__len__()) # 路径长度 探索/利用
+                    suc_len[i][1].append(env.cur_path.__len__()) # 路径长度 探索/利用
                     t_len[i] += env.cur_path.__len__()
 
                     if env.new_sln:
@@ -65,16 +63,16 @@ def DQNTest():
                            info = ("{}->{}".format(info, str(n)))
                         print("episode{} {}".format(episode, info))
                 else: # 没有抵达目的地
-                    suc_length[i][1].append(0)
+                    suc_len[i][1].append(0)
                 break
             DQN_step += 1
+        # enf of while(one trial)
 
         if episode % STAT_GAP == 0:
-            if suc_matrix[0][0] == 0 or suc_matrix[0][0] == STAT_GAP:
-                print('{} {}'.format(episode, suc_matrix[0][0]))
-                continue
-            c0 = round(suc_matrix[0][1] / suc_matrix[0][0], 4) # 探索成功率
-            c1 = round(suc_matrix[1][1] / suc_matrix[1][0], 4) # 利用成功率
+            # 探索成功率
+            c0 = 0 if suc_matrix[0][1] == 0 else round(suc_matrix[0][1] / suc_matrix[0][0], 4)
+            # 利用成功率
+            c1 = 0 if suc_matrix[1][1] == 0 else round(suc_matrix[1][1] / suc_matrix[1][0], 4)
             suc[0].append(c0)
             suc[1].append(c1)
 
@@ -93,10 +91,14 @@ def DQNTest():
 
             suc_matrix *= 0
             t_len *= 0
+    # enf of for(train process)
+    display(endpoints, x, suc, avr_len, suc_len)
 
+
+def display(endpoints, x, suc, avr_len, suc_len):
+    print('game over', time.ctime())
     print(endpoints)
     RL.plot_cost()
-    print('game over', time.ctime())
 
     fig, ax = plt.subplots(2, 2)
     # 成功率
@@ -108,8 +110,8 @@ def DQNTest():
     ax[0][1].plot(x, avr_len[1], 'b-')
 
     # 寻路情况
-    ax[1][0].plot(suc_length[0][0], suc_length[0][1], 'r.', linewidth = 0.10)
-    ax[1][0].plot(suc_length[1][0], suc_length[1][1], 'b.', linewidth = 0.10)
+    ax[1][0].plot(suc_len[0][0], suc_len[0][1], 'r.', linewidth = 0.10)
+    ax[1][0].plot(suc_len[1][0], suc_len[1][1], 'b.', linewidth = 0.10)
 
     # 终点热力图
     #endpoints[3][3] /= 8
